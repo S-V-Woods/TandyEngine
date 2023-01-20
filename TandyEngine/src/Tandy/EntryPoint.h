16a126dd5dd4610905789218e90cd86c1a544078
Ian Riley <isr413@utulsa.edu>
2023-01-20 09:10:32 -0600

    Initialize project4

---
+++ TandyEngine/src/Tandy/EntryPoint.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef TS_PLATFORM_WINDOWS
+
+	extern Tandy::Application* Tandy::CreateApplication();
+
+	int main(int argc, char** argv) {
+		// initialize logger
+		Tandy::Log::Init();
+		// TODO: (optional) can be rewritten to use a macro instead
+		Tandy::Log::GetLogger()->info("Tandy Engine Start!");
+		// start app
+		auto app = Tandy::CreateApplication();
+		// trigger app start event
+		app->OnEvent();
+		app->Run();
+		delete app;
+	}
+
+#endif
