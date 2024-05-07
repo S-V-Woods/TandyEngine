c775d56cf6b1c2ac6aa10040020a9a1a01fa8b43
S-V-Woods <symia.woodson@gmail.com>
2024-05-07 14:32:18 -0500

    Log.h and log.cpp
    Got log.h an

---
+++ TandyEngine/src/Tandy/Log.cpp
@@ -0,0 +1,14 @@
+#include "Log.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Tandy{
+
+	std::shared_ptr<spdlog::logger> Log::	logger;
+	void Log:: Init()
+	{
+		spdlog::set_pattern("%^[%T} %n: %v%$");
+		logger = spdlog::stdout_color_mt("TANDY");
+		logger->set_level(spdlog::level::trace);
+	}
+
+}
---
+++ TandyEngine/src/Tandy/Log.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Core.h"
+#include <memory>
+#include "spdlog/spdlog.h"
+
+namespace Tandy{
+	class TANDY_API Log
+	{
+
+	public:
+		static void Init();
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return logger; }
+	private:
+		static std::shared_ptr<spdlog::logger> logger;
+
+	};
+}
+//core log macros
+#define TS_TRACE(...)	::Tandy::Log:GetLogger()->trace(__VA_ARGS__)//two underlines
+#define TS_INFO(...)	::Tandy::Log:GetLogger()->info(__VA_ARGS__)
+#define TS_WARN(...)	::Tandy::Log:GetLogger()->warn(__VA_ARGS__)
+#define TS_ERROR(...)	::Tandy::Log:GetLogger()->error(__VA_ARGS__)
+#define TS_FATAL(...)	::Tandy::Log:GetLogger()->fatal(__VA_ARGS__)
